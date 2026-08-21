e79e75b54cd2d4f1e4bf0f2d9915bfacb6a4baee
sxdgdgd-ux <sxdgdgd@gmail.com>
2026-08-21 16:38:24 -0400

    sffd

---
+++ clase 14/empresa.sql
@@ -0,0 +1,5 @@
+-- Para crear una base de datos SQL, se utiliza la declaracion CREATE DATABASE.
+CREATE DATABASE empresa;
+
+-- El comando USE se utiliza para seleccionar la base de datos
+USE empresa;
